b85d3f43461f3b48b01b32415bb6da13882123b6
Tetsuya Yokose <gyakutentetsu@gmail.com>
2019-05-29 12:58:12 +0900

    CSS設定をmaterial-ui/stylesで行うように変更

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import { ThemeProvider, makeStyles } from '@material-ui/styles';
 import Particles from 'react-particles-js';
-import './css/client.min.css';
 
+const useStyles = makeStyles(theme => ({
+  app: {
+    background: theme.background,
+    width: '100vw',
+    height: '100vh',
+    margin: '0',
+    padding: '0',
+  }
+}));
 
+const theme = {
+  background: 'linear-gradient(135deg, #f989cb 0%, #7ee5e3 100%)',
+};
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme} >
+      <Main />
+    </ThemeProvider>
+  );
+}
+
+function Main() {
+  const classes = useStyles();
+  return (
+    <div className={classes.app}>
       <Particles />
     </div>
   );
+
 }
 
 export default App;
