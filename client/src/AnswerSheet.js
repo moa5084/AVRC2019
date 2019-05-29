2272819fdcdc8bc9343d5d340fc242651f891230
Tetsuya Yokose <gyakutentetsu@gmail.com>
2019-05-30 00:00:31 +0900

    AnswerSheetの基本形のスタイリングまでを実装

---
+++ client/src/AnswerSheet.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import SendIcon from '@material-ui/icons/Send';
+
+import { withStyles } from '@material-ui/styles';
+
+import 'typeface-roboto';
+
+const styles = (theme) => ({
+    AnswerSheet: {
+        width: '100%',
+        margin: '0',
+        padding: '20px',
+        background: 'rgba(255, 255, 255, 0.5)',
+        boxShadow: '3px 3px 1px 1px rgba(40, 40, 40, 0.2), inset 0 0 10px 0 #fff',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    },
+    Header: {
+        width: '100%',
+        textAlign: 'center',
+    },
+    QuestionWrapper: {
+        width: '90%',
+        minHeight: '60px',
+        position: 'relative',
+        padding: '10px',
+        '&::before': {
+            content: '""',
+            width: '20px',
+            height: '30px',
+            position: 'absolute',
+            display: 'inline-block',
+            top: '0',
+            left: '0',
+            borderLeft: '#999 1px solid',
+            borderTop: '#999 1px solid',
+        },
+        '&::after': {
+            content: '""',
+            width: '20px',
+            height: '30px',
+            position: 'absolute',
+            display: 'inline-block',
+            bottom: '0',
+            right: '0',
+            borderRight: '#999 1px solid',
+            borderBottom: '#999 1px solid',
+        },
+    },
+    img: {
+        width: '100%',
+        height: 'auto'
+    },
+    Form: {
+        margin: '40px 0',
+        width: '100%',
+        display: 'flex',
+    },
+    answerField: {
+        flexGrow: '5',
+    },
+    submitButton: {
+        flexGrow: '1',
+        margin: '0 3%',
+    },
+    Footer: {
+        width: '100%',
+        minHeight: '10px',
+        textAlign: 'center',
+    },
+    footerImg: {
+        maxWidth: '40%',
+        height: 'auto',
+    },
+});
+
+class AnswerSheet extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            answer: '',
+        }
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({answer: e.target.value});
+    }
+
+    render () {
+        return (
+            <div className={this.props.classes.AnswerSheet}>
+                <div className={this.props.classes.Header}>
+                    <Typography variant="h2" gutterBottom>
+                        A
+                    </Typography>
+                </div>
+                <div className={this.props.classes.QuestionWrapper}>
+                    <img src='http://juicy-apple.fun/av/AVRC2019/images/Final-2.pdf' alt='loading' className={this.props.classes.img}/>
+                </div>
+                <div className={this.props.classes.Form}>
+                    <TextField 
+                        id='answer'
+                        label='答えを入力'
+                        className={this.props.classes.answerField}
+                        variant='outlined'
+                        value={this.state.answer}
+                        onChange={this.onChange}
+                    />      
+                    <Button variant="contained" color="primary" className={this.props.classes.submitButton}>
+                        <SendIcon />
+                    </Button>
+                </div>
+                <div className={this.props.classes.Footer}>
+                    <img src='http://juicy-apple.fun/av/AVRC2019/images/AVRC2018_Logo.png' alt='loading' className={this.props.classes.footerImg}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(AnswerSheet);
--- client/src/App.js
+++ client/src/App.js
@@ -2,36 +2,82 @@ import React from 'react';
 import { ThemeProvider, makeStyles } from '@material-ui/styles';
 import Particles from 'react-particles-js';
 
+import AnswerSheet from './AnswerSheet';
+import { createMuiTheme } from '@material-ui/core';
+
 const useStyles = makeStyles(theme => ({
-  app: {
+  wrapper: {
     background: theme.background,
     width: '100vw',
     height: '100vh',
     margin: '0',
     padding: '0',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  main: {
+    width: '90%',
+    height: '90%',
+    margin: '0',
+    padding: '0',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  particles: {
+    width: '100vw',
+    height: '100vh',
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    xIndex: '-1',
   }
 }));
 
-const theme = {
+const theme = createMuiTheme({
   background: 'linear-gradient(135deg, #f989cb 0%, #7ee5e3 100%)',
-};
+  palette: {
+    primary: {
+      light: '#ff669a',
+      main: '#ff4081',
+      dark: '#b22c5a',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#66cfff',
+      main: '#40c4ff',
+      dark: '#2c89b2',
+      contrastText: '#000',
+    },
+  },
+});
 
 function App() {
   return (
     <ThemeProvider theme={theme} >
-      <Main />
+      <Wrapper />
     </ThemeProvider>
+  );
+}
+
+function Wrapper() {
+  const classes = useStyles();
+  return (
+    <div className={classes.wrapper}>
+      <Particles className={classes.particles} />
+      <Main />
+    </div>
   );
 }
 
 function Main() {
   const classes = useStyles();
   return (
-    <div className={classes.app}>
-      <Particles />
+    <div className={classes.main}>
+      <AnswerSheet />
     </div>
   );
-
 }
 
 export default App;
