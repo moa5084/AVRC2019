e22398a7730dcbe9480757530926f4a6256da18e
Tetsuya Yokose <gyakutentetsu@gmail.com>
2019-06-04 23:14:14 +0900

    State管理を開始

--- client/src/Player.js
+++ client/src/Player.js
@@ -9,42 +9,53 @@ import questions from './questions';
 import Header from './Header';
 import Styles from './Styles';
 
-function searchQuestion (id) {
-    let res = [];
-    questions.forEach(round => {
-        round.questions.forEach(q => {
-            if (q.id === id) res.push(q);
-        });
-    });
-    return (res.length > 0 ? res[0] : null);
-}
-  
-function searchRound (id) {
-    let res = [];
-    questions.forEach(round => {
-        if (round.roundid === id) res.push(round);
-    });
-    return (res.length > 0 ? res[0] : null);
-}
-
 class Player extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            team: {
+                teamName: 'アナザーピジョン',
+                members: ['アナザー', 'ピジョン'],
+            },
+            questions: questions,
+        }
+    }
+
+    searchQuestion (id) {
+        let res = [];
+        this.state.questions.forEach(round => {
+            round.questions.forEach(q => {
+                if (q.id === id) res.push(q);
+            });
+        });
+        return (res.length > 0 ? res[0] : null);
+    }
+
+    searchRound (id) {
+        let res = [];
+        this.state.questions.forEach(round => {
+            if (round.roundid === id) res.push(round);
+        });
+        return (res.length > 0 ? res[0] : null);
+    }
+
     render () {
         const prefix = this.props.type === 'A' ? '/Newcomer' : '/NewComer';
         return (
             <Router basename='/tokusetsu/party2019'>
-                <Header questions={questions}/>
+                <Header questions={this.state.questions}/>
                 <Switch>
                     <Route exact path={prefix} render={(props) => {
                         return false;
                     }}/>
                     <Route exact path={prefix + '/:round'} render={(props) => {
-                        const myRound = searchRound(props.match.params.round);
+                        const myRound = this.searchRound(props.match.params.round);
                         return (
                             <RoundMenu round={myRound} />
                         );
                     }}/>
                     <Route exact path={prefix + '/:round/:question'} render={(props) => {
-                        const myQuestion = searchQuestion(props.match.params.question);
+                        const myQuestion = this.searchQuestion(props.match.params.question);
                         return (
                             <AnswerSheet question={myQuestion} />
                         );
