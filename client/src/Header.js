4fcb984c6bf522f826e41ab97dc4fab6c63e8b7d
Tetsuya Yokose <gyakutentetsu@gmail.com>
2019-06-06 19:04:05 +0900

    Team情報が追加されている状態で表示をSwitch可能に

--- client/src/Header.js
+++ client/src/Header.js
@@ -4,21 +4,63 @@ import { Route, Switch, Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
 import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
 import PersonIcon from '@material-ui/icons/Person';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import PeopleIcon from '@material-ui/icons/People';
 
 import Styles from './Styles';
 
 class Header extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            index: 0,
+        }
+        this.clickHandler = this.clickHandler.bind(this);
+    }
+
+    clickHandler () {
+        if (!this.props.team.visibility) {
+            this.setState({index: 0});
+        } else {
+            let ind = this.state.index;
+            ind = ++ind % 3;
+            this.setState({index: ind});
+        }
+    }
+
     render () {
-        const teamData = this.props.team.visibility ? (
-            <div className={this.props.classes.HeaderTeamWrapper}>
-                <PeopleIcon />{this.props.team.teamName}
-            </div>
-        ) : (
-            <div className={this.props.classes.HeaderTeamWrapper}>
-                <PersonIcon />{this.props.team.myName}
-            </div>
-        );
+        let teamData;
+        if (this.props.team.visibility) {
+            switch (this.state.index) {
+                case 1:
+                    teamData = (
+                        <div className={this.props.classes.HeaderTeamWrapper} onClick={() => {this.clickHandler()}}>
+                            <PersonIcon />{this.props.team.myName}
+                        </div>
+                    );
+                    break;
+                case 2:
+                    teamData = (
+                        <div className={this.props.classes.HeaderTeamWrapper} onClick={() => {this.clickHandler()}}>
+                            <PersonAddIcon />{this.props.team.teammateName}
+                        </div>
+                    );
+                    break;
+                default:
+                    teamData = (
+                        <div className={this.props.classes.HeaderTeamWrapper} onClick={() => {this.clickHandler()}}>
+                            <PeopleIcon />{this.props.team.teamName}
+                        </div>
+                    );
+                    break;
+            }
+        } else {
+            teamData = (
+                <div className={this.props.classes.HeaderTeamWrapper}>
+                    <PersonIcon />{this.props.team.myName}
+                </div>
+            );
+        }
         return (
             <div className={this.props.classes.Header}>
                 <div className={this.props.classes.HeaderBackWrapper}>
