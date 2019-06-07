992bdf3e1a6d68f6f881ba64f1def33a9d039d01
Tetsuya Yokose <gyakutentetsu@gmail.com>
2019-06-08 02:34:43 +0900

    RankingDisplayをInitialize

---
+++ client/src/RankingDisplay.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import { withStyles } from '@material-ui/styles';
+
+const styles = (theme) => {
+
+}
+
+class RankingDisplay extends Component {
+    render () {
+        return (
+            <div className={this.props.classes.RankingDisplay}>
+                
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(RankingDisplay);
